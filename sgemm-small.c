3553d72c4bef48a50e71be0478c579fa741ea0ce
Paden Tomasello <padent@berkeley.edu>
2013-04-16 13:49:42 -0700

    Pad all

--- sgemm-small.c
+++ sgemm-small.c
@@ -15,8 +15,8 @@ void sgemm( int m, int n, int d, float *A, float *C )
 	int ps=n;
 //	printf("test11, n: %d, m: %d\n", n, m);
 
-	if ( (n%4) != 0) {
-		ps = ((n/4)+1)*4;
+	if ( (n%20) != 0) {
+		ps = ((n/20)+1)*20;
 //		printf("\n\n ps=%d\n\n", ps);
 		A2 = (float*) malloc( (n+m) * ps * sizeof(float) );
 		C2 = (float*) malloc( ps * ps * sizeof(float) );
@@ -67,34 +67,34 @@ void sgemm( int m, int n, int d, float *A, float *C )
 			_mm_store_ps((C2+(i*ps)+(4*(k+4))), c5);
 		}
 //		printf("test17, i: %d, k: %d, ps:%d\n", i, k, ps);
-		for (; k+1 < ps/4; k+=2) { //Goes through 4 rows at a time of C and A.
-			c1 = _mm_setzero_ps();
-			c2 = c1;
-			for (int j = 0; j < m; j+=1) { //Goes through Goes through width of m, data strip.
-				b = _mm_load1_ps((A2+(j*ps)+(i*(ps+1))));
-				a1 = _mm_load_ps((A2+(k*4)+(j*ps)));
-				a2 = _mm_load_ps((A2+((k+1)*4)+(j*ps)));
-				c2 = _mm_add_ps(_mm_mul_ps(a2,b), c2);
-				c1 = _mm_add_ps(_mm_mul_ps(a1,b), c1);
-			}
-			_mm_store_ps((C2+(i*ps)+(4*k)), c1);
-			_mm_store_ps((C2+(i*ps)+(4*(k+1))), c2);
-		}
-	//	printf("test18, i: %d, k: %d, ps:%d\n", i, k, ps);
-		if (k < ps/4) {
-			c1 = _mm_setzero_ps();
-			for (int j = 0; j < m; j+=1) { //Goes through Goes through width of m, data strip.
-
-				b = _mm_load1_ps((A2+(j*ps)+(i*(ps+1))));
-				a1 = _mm_load_ps((A2+(k*4)+(j*ps)));
-				c1 = _mm_add_ps(_mm_mul_ps(a1,b), c1);
-
-			}
-		//	printf("test16\n");
-			_mm_store_ps((C2+(i*ps)+(4*k)), c1);
-
-		}
-	}
+//		for (; k+1 < ps/4; k+=2) { //Goes through 4 rows at a time of C and A.
+//			c1 = _mm_setzero_ps();
+//			c2 = c1;
+//			for (int j = 0; j < m; j+=1) { //Goes through Goes through width of m, data strip.
+//				b = _mm_load1_ps((A2+(j*ps)+(i*(ps+1))));
+//				a1 = _mm_load_ps((A2+(k*4)+(j*ps)));
+//				a2 = _mm_load_ps((A2+((k+1)*4)+(j*ps)));
+//				c2 = _mm_add_ps(_mm_mul_ps(a2,b), c2);
+//				c1 = _mm_add_ps(_mm_mul_ps(a1,b), c1);
+//			}
+//			_mm_store_ps((C2+(i*ps)+(4*k)), c1);
+//			_mm_store_ps((C2+(i*ps)+(4*(k+1))), c2);
+//		}
+//	//	printf("test18, i: %d, k: %d, ps:%d\n", i, k, ps);
+//		if (k < ps/4) {
+//			c1 = _mm_setzero_ps();
+//			for (int j = 0; j < m; j+=1) { //Goes through Goes through width of m, data strip.
+//
+//				b = _mm_load1_ps((A2+(j*ps)+(i*(ps+1))));
+//				a1 = _mm_load_ps((A2+(k*4)+(j*ps)));
+//				c1 = _mm_add_ps(_mm_mul_ps(a1,b), c1);
+//
+//			}
+//		//	printf("test16\n");
+//			_mm_store_ps((C2+(i*ps)+(4*k)), c1);
+//
+//		}
+//	}
  //   printf("test14\n");
 	if ( (n%4) != 0) {
 		for (int i = 0; i < n; i++) {
